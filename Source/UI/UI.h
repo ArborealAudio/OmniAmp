17dc06ab3a020310db1f5f0fb488fa8ad6121ea5
Arboreal Audio <arborealaudio@protonmail.com>
2022-08-06 08:51:29 -0400

    create macros for some global colors

--- Source/UI/UI.h
+++ Source/UI/UI.h
@@ -2,7 +2,11 @@
 // header for including all UI-related files
 
 #pragma once
-// #include "Background.h"
+
+#define BACKGROUND_COLOR 0xffaa8875
+#define AMP_COLOR 0xffcab4a0
+#define TOP_TRIM 0xff293236
+
 #include "Button.h"
 #include "ChoiceMenu.h"
 #include "Knob.h"
