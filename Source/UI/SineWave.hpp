5c963fe92c47768dbd29ed8abdae332ca190c53a
Arboreal Audio <arborealaudio@protonmail.com>
2022-08-11 13:47:20 -0400

    start refactor of sinewave component

--- Source/UI/SineWave.hpp
+++ Source/UI/SineWave.hpp
@@ -119,32 +119,90 @@ struct SineWaveComponent : Component, Timer
 {
     SineWaveComponent(AudioSource& s) : src(s)
     {
-        startTimerHz(60);
+        startTimerHz(45);
     }
     ~SineWaveComponent(){ stopTimer(); }
 
     void paint(Graphics& g) override
     {
-        g.setColour(Colour(0xff363536));
+        g.setColour(Colour(TOP_TRIM).withAlpha(0.33f));
         g.fillRoundedRectangle(getLocalBounds().toFloat(), 5.f);
 
         drawSineWave(g);
+    }
 
-        // g.setColour(Colours::black);
-        // g.drawRoundedRectangle(getLocalBounds().toFloat(), 5.f, 3.f);
+    void resized() override
+    {
+        for (auto& i : wave)
+            i = Image(Image::PixelFormat::ARGB, getWidth() * 2, getHeight(), true);
+        
+        Graphics g[6]
+        {
+            Graphics(wave[0]),
+            Graphics(wave[1]),
+            Graphics(wave[2]),
+            Graphics(wave[3]),
+            Graphics(wave[4]),
+            Graphics(wave[5])
+        };
+
+        auto w = getWidth();
+        float min = getLocalBounds().getBottom();
+        float max = getLocalBounds().getY();
+
+        auto map = [min, max](float input)
+        {
+            return jmap(input, -1.f, 1.f, min, max);
+        };
+
+        Path p[6];
+
+        for (auto& q : p)
+            q.startNewSubPath(-1, getLocalBounds().getCentreY());
+
+        for (float i = -w; i < w; i += 1.f)
+        {
+            p[0].lineTo(i, map(std::sin(i * 0.025f)));
+            p[1].lineTo(i, map(std::sin(i * 0.0275f)));
+            p[2].lineTo(i, map(std::sin(i * 0.03f)));
+            p[3].lineTo(i, map(std::sin(i * 0.0325f)));
+            p[4].lineTo(i, map(std::sin(i * 0.035f)));
+            p[5].lineTo(i, map(std::sin(i * 0.0375f)));
+        }
+
+        g[5].setColour(Colour(0xff975792));
+        g[5].strokePath(p[5], PathStrokeType(0.75f));
+        g[4].setColour(Colour(0xff516692));
+        g[4].strokePath(p[4], PathStrokeType(1.f));
+        g[3].setColour(Colour(0xff499481));
+        g[3].strokePath(p[3], PathStrokeType(1.25f));
+        g[2].setColour(Colour(0xff859358));
+        g[2].strokePath(p[2], PathStrokeType(2.5f));
+        g[1].setColour(Colour(0xff977757));
+        g[1].strokePath(p[1], PathStrokeType(2.75f));
+        g[0].setColour(Colour(0xff975755));
+        g[0].strokePath(p[0], PathStrokeType(3.f));
     }
 
     void drawSineWave(Graphics& g)
     {
         if (!needsRepaint)
             return;
-        
+
+        auto bounds = getLocalBounds().toFloat();
         auto w = getWidth();
-        // std::vector<float> wave;
-        // wave.resize(w);
-        // src.readBuffer(wave);
+        auto h = getHeight();
+
+        auto f = phase.advance(0.05f); // inc btw 0->2Pi
+        auto fw = (f * w) / MathConstants<float>::twoPi; // width section represented by f
+
         auto rms = src.getAvgRMS();
 
+        // for (int i = 0; i < 6; ++i)
+        // {
+        //     g.drawImage(wave[i], bounds.withX(fw), RectanglePlacement::fillDestination);
+        //     g.drawImage(wave[i], bounds.withRightX(fw), RectanglePlacement::fillDestination);
+        // }
         float min = getLocalBounds().getBottom();
         float max = getLocalBounds().getY();
 
@@ -160,12 +218,12 @@ struct SineWaveComponent : Component, Timer
 
         for (int i = 0; i < w; ++i)
         {
-            p[0].lineTo(i, map(std::sin(i * 0.025f  - f ) * rms[0]));
-            p[1].lineTo(i, map(std::sin(i * 0.0275f - f ) * rms[1] /*  * 0.9f */));
-            p[2].lineTo(i, map(std::sin(i * 0.03f   - f ) * rms[2]   /*  * 0.8f */));
-            p[3].lineTo(i, map(std::sin(i * 0.0325f - f ) * rms[3] /*  * 0.7f */));
-            p[4].lineTo(i, map(std::sin(i * 0.035f  - f ) * rms[4]  /*  * 0.6f */));
-            p[5].lineTo(i, map(std::sin(i * 0.0375f - f ) * rms[5] /*  * 0.5f */));
+            p[0].lineTo(i, map(std::sin(i * 0.025f  - f) * rms[0]));
+            p[1].lineTo(i, map(std::sin(i * 0.0275f - f) * rms[1]));
+            p[2].lineTo(i, map(std::sin(i * 0.03f   - f) * rms[2]));
+            p[3].lineTo(i, map(std::sin(i * 0.0325f - f) * rms[3]));
+            p[4].lineTo(i, map(std::sin(i * 0.035f  - f) * rms[4]));
+            p[5].lineTo(i, map(std::sin(i * 0.0375f - f) * rms[5]));
         }
 
         g.setColour(Colour(0xff975792));
@@ -181,7 +239,6 @@ struct SineWaveComponent : Component, Timer
         g.setColour(Colour(0xff975755));
         g.strokePath(p[0], PathStrokeType(3.f));
 
-        f += 0.05;
         needsRepaint = false;
     }
 
@@ -197,8 +254,10 @@ struct SineWaveComponent : Component, Timer
 private:
     AudioSource &src;
 
-    float f = 0.f;
+    dsp::Phase<float> phase;
     bool needsRepaint = false;
+
+    Image wave[6];
 };
 
 } // namespace strix
