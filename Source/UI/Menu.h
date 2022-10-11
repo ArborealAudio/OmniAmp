a6d086ae2b4b69df01614ac02683b3d47435136a
Alex Riccio <alexbriccio@gmail.com>
2022-10-11 15:42:10 -0400

    adjustments to menu UI (unfinished)

--- Source/UI/Menu.h
+++ Source/UI/Menu.h
@@ -15,28 +15,77 @@ class MenuComponent : public Component
 
     struct ButtonList : Component
     {
+        enum ButtonType
+        {
+            Toggle,
+            PrePost,
+            Default
+        };
+
         struct ListButton : TextButton
         {
+            ListButton()
+            {
+                type = ButtonType::Default;
+            }
+
+            ListButton(ButtonType newType) : type(newType)
+            {
+            }
+
             void paint(Graphics& g) override
             {
-                if (isMouseOver()) {
+                if (isMouseOver())
                     g.setColour(Colours::lightslategrey);
-                }
 
                 g.fillRect(getLocalBounds());
 
                 auto text = getButtonText();
-                g.setColour(Colours::white);
-                if (getToggleState()) {
-                    AttributedString checked;
-                    checked.setText(String::fromUTF8("✅ ") + text);
-                    checked.setJustification(Justification::centred);
-                    checked.setColour(Colours::white);
-                    checked.draw(g, getLocalBounds().toFloat());
+
+                auto bounds = getLocalBounds();
+                auto textBounds = bounds;
+
+                switch (type)
+                {
+                case ButtonType::Toggle:
+                    textBounds = bounds.removeFromLeft((float)bounds.getWidth() * 0.75f);
+                    bounds.reduce(2, 10);
+                    if (getToggleState()) {
+                        g.setColour(Colours::seagreen);
+                        g.fillRoundedRectangle(bounds.toFloat(), 5.f);
+                        g.setColour(Colours::white);
+                        g.fillEllipse(bounds.removeFromRight(bounds.getWidth() / 2).toFloat());
+                    }
+                    else {
+                        g.setColour(Colours::darkgrey);
+                        g.fillRoundedRectangle(bounds.toFloat(), 5.f);
+                        g.setColour(Colours::white);
+                        g.fillEllipse(bounds.removeFromLeft(bounds.getWidth() / 2).toFloat());
+                    }
+                    g.setColour(Colours::white);
+                    g.drawFittedText(text, textBounds, Justification::centred, 1);
+                    break;
+                case ButtonType::PrePost:
+                    textBounds = bounds.removeFromLeft((float)bounds.getWidth() * 0.75f);
+                    bounds.reduce(0, 5);
+                    g.setColour(Colours::white);
+                    g.fillRoundedRectangle(bounds.toFloat(), 5.f);
+                    g.setColour(Colours::black);
+                    if (getToggleState())
+                        g.drawFittedText("Post", bounds, Justification::centred, 1);
+                    else
+                        g.drawFittedText("Pre", bounds, Justification::centred, 1);
+                    g.setColour(Colours::white);
+                    g.drawFittedText(text, textBounds, Justification::centred, 1);
+                    break;
+                case ButtonType::Default:
+                    g.setColour(Colours::white);
+                    g.drawFittedText(text, textBounds, Justification::centred, 1);
+                    break;
                 }
-                else
-                    g.drawFittedText(text, getLocalBounds(), Justification::centred, 1);
             }
+        private:
+            ButtonType type;
         };
 
         ButtonList()
@@ -62,6 +111,9 @@ class MenuComponent : public Component
 
             compLink.setButtonText("Comp Stereo Link");
             compLink.setClickingTogglesState(true);
+
+            compPos.setButtonText("Comp Pos: ");
+            compPos.setClickingTogglesState(true);
 
             windowSize.setButtonText("Default UI size");
             windowSize.setClickingTogglesState(false);
@@ -90,11 +142,11 @@ class MenuComponent : public Component
 
         std::function<void(CommandID, bool)> onItemClick;
 
-        ListButton HQ, renderHQ, compLink;
+        ListButton HQ{ButtonType::Toggle}, renderHQ{ButtonType::Toggle}, compLink{ButtonType::Toggle}, compPos{ButtonType::PrePost};
 
     private:
     #if JUCE_WINDOWS || JUCE_LINUX
-        ListButton openGL;
+        ListButton openGL{ButtonType::Toggle};
     #endif
         ListButton windowSize, checkUpdate;
 
@@ -107,6 +159,7 @@ class MenuComponent : public Component
                 &HQ,
                 &renderHQ,
                 &compLink,
+                &compPos,
                 &windowSize,
                 &checkUpdate
             };
@@ -121,16 +174,20 @@ class MenuComponent : public Component
 
     SidePanel panel;
 
-    std::unique_ptr<AudioProcessorValueTreeState::ButtonAttachment> hqAttach, renderHQAttach, compLinkAttach;
+    std::unique_ptr<AudioProcessorValueTreeState::ButtonAttachment> hqAttach, renderHQAttach, compLinkAttach, compPosAttach;
 
 public:
-    MenuComponent(AudioProcessorValueTreeState& a, int width) : vts(a), panel("Options", width, false), menuButton("Menu", DrawableButton::ButtonStyle::ImageFitted)
+    MenuComponent(AudioProcessorValueTreeState& a, int width) : vts(a), panel("", width, false), menuButton("Menu", DrawableButton::ButtonStyle::ImageFitted)
     {
         addAndMakeVisible(menuButton);
         icon = Drawable::createFromImageData(BinaryData::Hamburger_icon_svg, BinaryData::Hamburger_icon_svgSize);
         menuButton.setImages(icon.get());
         menuButton.onClick = [&]
-        { panel.showOrHide(!panel.isPanelShowing()); };
+        {
+            panel.showOrHide(!panel.isPanelShowing());
+            if (menuClicked)
+                menuClicked(panel.isPanelShowing());
+        };
 
         addAndMakeVisible(panel);
         panel.setColour(SidePanel::ColourIds::backgroundColour, Colours::white);
@@ -161,6 +218,7 @@ public:
         hqAttach = std::make_unique<AudioProcessorValueTreeState::ButtonAttachment>(vts, "hq", list.HQ);
         renderHQAttach = std::make_unique<AudioProcessorValueTreeState::ButtonAttachment>(vts, "renderHQ", list.renderHQ);
         compLinkAttach = std::make_unique<AudioProcessorValueTreeState::ButtonAttachment>(vts, "compLink", list.compLink);
+        compPosAttach = std::make_unique<AudioProcessorValueTreeState::ButtonAttachment>(vts, "compPos", list.compPos);
     }
 
     ~MenuComponent() override
@@ -168,6 +226,7 @@ public:
         panel.setContent(nullptr);
     }
 
+    std::function<void(bool)> menuClicked;
     std::function<void()> windowResizeCallback;
     std::function<void()> checkUpdateCallback;
     std::function<void(bool)> openGLCallback;
