3e07177bae306b55c3a75d83f93ce06504d3298a
Arboreal Audio <arborealaudio@protonmail.com>
2022-10-13 14:07:07 -0400

    move read/write config to static global location

--- Source/UI/UI.h
+++ Source/UI/UI.h
@@ -15,6 +15,36 @@ static const Typeface::Ptr getCustomFont()
     return typeface;
 }
 
+static void writeConfigFile(const String& property, int value)
+{
+    File config {File::getSpecialLocation(File::userApplicationDataDirectory).getFullPathName() + "/Arboreal Audio/Gamma/config.xml"};
+    if (!config.existsAsFile())
+        config.create();
+    
+    auto xml = parseXML(config);
+    if (xml == nullptr)
+    {
+        xml.reset(new XmlElement("Config"));
+    }
+
+    xml->setAttribute(property, value);
+    xml->writeTo(config);
+}
+
+/*returns integer value of read property, or -1 if it doesn't exist*/
+static int readConfigFile(const String& property)
+{
+    File config {File::getSpecialLocation(File::userApplicationDataDirectory).getFullPathName() + "/Arboreal Audio/Gamma/config.xml"};
+    if (!config.existsAsFile())
+        return -1;
+    
+    auto xml = parseXML(config);
+    if (xml != nullptr && xml->hasTagName("Config"))
+        return xml->getIntAttribute(property, -1);
+    
+    return -1;
+}
+
 #include "Button.h"
 #include "ChoiceMenu.h"
 #include "Knob.h"
