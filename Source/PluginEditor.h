58e8d2897161287215f831a1250a7742405c8d0f
Arboreal Audio <arborealaudio@protonmail.com>
2022-10-13 14:08:06 -0400

    moved read/write config elsewhere

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -29,36 +29,6 @@ public:
 
 private:
 
-    void writeConfigFile(const String& property, int value)
-    {
-        File config {File::getSpecialLocation(File::userApplicationDataDirectory).getFullPathName() + "/Arboreal Audio/Gamma/config.xml"};
-        if (!config.existsAsFile())
-            config.create();
-        
-        auto xml = parseXML(config);
-        if (xml == nullptr)
-        {
-            xml.reset(new XmlElement("Config"));
-        }
-
-        xml->setAttribute(property, value);
-        xml->writeTo(config);
-    }
-
-    /*returns integer value of read property, or -1 if it doesn't exist*/
-    int readConfigFile(const String& property)
-    {
-        File config {File::getSpecialLocation(File::userApplicationDataDirectory).getFullPathName() + "/Arboreal Audio/Gamma/config.xml"};
-        if (!config.existsAsFile())
-            return -1;
-        
-        auto xml = parseXML(config);
-        if (xml != nullptr && xml->hasTagName("Config"))
-            return xml->getIntAttribute(property, -1);
-        
-        return -1;
-    }
-
     GammaAudioProcessor& audioProcessor;
 
     std::unique_ptr<Drawable> logo;
