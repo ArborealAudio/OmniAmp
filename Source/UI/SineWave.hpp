12a6b917a4d854eb7b07eabfbdb2bb5477e0ac6e
Arboreal Audio <arborealaudio@protonmail.com>
2022-10-22 14:45:51 -0400

    revert to faster sine drawing (no thread pool)

--- Source/UI/SineWave.hpp
+++ Source/UI/SineWave.hpp
@@ -132,60 +132,7 @@ struct SineWaveComponent : Component, Timer
         drawSineWave(g);
     }
 
-    void resized() override
-    {
-        // for (auto& i : wave)
-        //     i = Image(Image::PixelFormat::ARGB, getWidth() * 2, getHeight(), true);
-        
-        // Graphics g[6]
-        // {
-        //     Graphics(wave[0]),
-        //     Graphics(wave[1]),
-        //     Graphics(wave[2]),
-        //     Graphics(wave[3]),
-        //     Graphics(wave[4]),
-        //     Graphics(wave[5])
-        // };
-
-        // auto w = getWidth();
-        // float min = getLocalBounds().getBottom();
-        // float max = getLocalBounds().getY();
-
-        // auto map = [min, max](float input)
-        // {
-        //     return jmap(input, -1.f, 1.f, min, max);
-        // };
-
-        // Path p[6];
-
-        // for (auto& q : p)
-        //     q.startNewSubPath(-1, getLocalBounds().getCentreY());
-
-        // for (float i = -w; i < w; i += 1.f)
-        // {
-        //     p[0].lineTo(i, map(std::sin(i * 0.025f)));
-        //     p[1].lineTo(i, map(std::sin(i * 0.0275f)));
-        //     p[2].lineTo(i, map(std::sin(i * 0.03f)));
-        //     p[3].lineTo(i, map(std::sin(i * 0.0325f)));
-        //     p[4].lineTo(i, map(std::sin(i * 0.035f)));
-        //     p[5].lineTo(i, map(std::sin(i * 0.0375f)));
-        // }
-
-        // g[5].setColour(Colour(0xff975792));
-        // g[5].strokePath(p[5], PathStrokeType(0.75f));
-        // g[4].setColour(Colour(0xff516692));
-        // g[4].strokePath(p[4], PathStrokeType(1.f));
-        // g[3].setColour(Colour(0xff499481));
-        // g[3].strokePath(p[3], PathStrokeType(1.25f));
-        // g[2].setColour(Colour(0xff859358));
-        // g[2].strokePath(p[2], PathStrokeType(2.5f));
-        // g[1].setColour(Colour(0xff977757));
-        // g[1].strokePath(p[1], PathStrokeType(2.75f));
-        // g[0].setColour(Colour(0xff975755));
-        // g[0].strokePath(p[0], PathStrokeType(3.f));
-    }
-
-    void drawSineWave(Graphics& g)
+    inline void drawSineWave(Graphics& g)
     {
         if (!needsRepaint)
             return;
@@ -217,15 +164,15 @@ struct SineWaveComponent : Component, Timer
             for (int j = 0; j < w; ++j)
                 x.emplace_back(j);
             FloatVectorOperations::multiply(x.data(), scale[i], x.size());
-            FloatVectorOperations::add(x.data(), -f, x.size());
+            FloatVectorOperations::add(x.data(), -f, x.size()); /*much faster w/ float vec ops!*/
             for (int j = 0; j < w; ++j)
                 p[i].lineTo(j, map(std::sin(x[j]) * rms[i]));
         };
 
-        ThreadPool pool{6};
-        pool.setThreadPriorities(7);
-
-        gin::multiThreadedFor<int>(0, 6, 1, &pool, drawWave);
+        for (int i = 0; i < 6; ++i)
+        {
+            drawWave(i);
+        }
 
         g.setColour(Colour(0xff975792));
         g.strokePath(p[5], PathStrokeType(0.75f));
@@ -257,8 +204,6 @@ private:
 
     dsp::Phase<float> phase;
     bool needsRepaint = false;
-
-    // Image wave[6];
 };
 
 } // namespace strix
