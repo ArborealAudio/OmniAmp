ed36b818b8d9b688344ce3eb5adc62867137a884
Alex Riccio <alexbriccio@gmail.com>
2022-11-10 09:56:04 -0500

    edit install scripts a bit for different builds

--- scripts/win-installer.sh
+++ scripts/win-installer.sh
@@ -6,14 +6,17 @@ set -e
 
 name=$1
 version=$2
+build=$3
+[ $build = "" ] && $build = "Release"
+
 parent_dir=~/${name}_install/$version
 
 [ ! -d "$parent_dir" ] && mkdir -p $parent_dir
 
 echo "Copying binaries..."
 
-cp -r ../build/${name}_artefacts/Release/VST/$name.dll $parent_dir
-cp -r ../build/${name}_artefacts/Release/VST3/$name.vst3 $parent_dir
+cp -r ../build/${name}_artefacts/$build/VST/$name.dll $parent_dir
+cp -r ../build/${name}_artefacts/$build/VST3/$name.vst3 $parent_dir
 cp ../manual/${name}.pdf $parent_dir/${name}_manual.pdf
 
 echo "Exited with code $?"
