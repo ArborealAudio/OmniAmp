61c413009d9ab8b5075a0f79d64bd02752975001
Arboreal Audio <arborealaudio@protonmail.com>
2022-12-22 14:29:25 -0500

    begin impl. new cab UI

--- Source/UI/CabComponent.h
+++ Source/UI/CabComponent.h
@@ -60,38 +60,47 @@ private:
         {
             micPos = static_cast<strix::FloatParameter *>(a.getParameter("cabMicPosX"));
             micDepth = static_cast<strix::FloatParameter *>(a.getParameter("cabMicPosZ"));
+
+            mic_img = Drawable::createFromImageData(BinaryData::mic_svg, BinaryData::mic_svgSize);
+            spkr_img = Drawable::createFromImageData(BinaryData::speaker_svg, BinaryData::speaker_svgSize);
         }
 
         void paint(Graphics &g) override
         {
-            auto bounds = getLocalBounds().reduced(2);
+            auto bounds = getLocalBounds().reduced(5);
             g.setColour(Colours::black.withAlpha(0.25f));
             g.fillRoundedRectangle(bounds.toFloat(), 5.f);
 
-            const float pointWidth = 10.f;
+            clampPoints();
+            Rectangle<float> micPoint(posX, posY, micWidth, micWidth);
+            mic_img->replaceColour(Colours::black, Colours::white);
+            mic_img->drawWithin(g, micPoint, RectanglePlacement::centred, 1.f);
 
-            Rectangle<float> micPoint(posX, posY, pointWidth, pointWidth);
-            g.setColour(Colours::white);
-            g.fillEllipse(micPoint);
+            spkr_img->replaceColour(Colours::black, Colours::white);
+            spkr_img->drawWithin(g, spkrBounds, RectanglePlacement::centred, 1.f);
+        }
+
+        void clampPoints()
+        {
+            posX = jlimit(micBounds.getX(), micBounds.getRight() - micWidth, posX);
+            posY = jlimit(micBounds.getY(), micBounds.getBottom() - micWidth, posY);
         }
 
         void mouseDrag(const MouseEvent &event) override
         {
             if (event.eventComponent == this)
             {
-                auto x = event.position.getX();
-                auto y = event.position.getY();
+                posX = event.position.x;
+                posY = event.position.y;
+                auto xFrac = posX / micBounds.getWidth();
+                auto yFrac = posY / micBounds.getHeight();
+                DBG("x: " << xFrac);
+                DBG("y: " << yFrac);
 
-                auto bounds = getLocalBounds().reduced(2);
+                apvts.getParameterAsValue("cabMicPosX") = xFrac;
+                apvts.getParameterAsValue("cabMicPosZ") = yFrac;
 
-                posX = x;
-                posY = y;
-
-                apvts.getParameterAsValue("cabMicPosX") = posX / bounds.getWidth();
-                apvts.getParameterAsValue("cabMicPosZ") = posY / bounds.getHeight();
-
-                posX = jlimit(7.f, bounds.getWidth() - 12.f, posX);
-                posY = jlimit(7.f, bounds.getHeight() - 12.f, posY);
+                clampPoints();
 
                 repaint();
             }
@@ -103,9 +112,9 @@ private:
             {
                 apvts.getParameterAsValue("cabMicPosX") = 0.5f;
                 apvts.getParameterAsValue("cabMicPosZ") = 1.f;
-                auto bounds = getLocalBounds().reduced(2);
-                posX = bounds.getWidth() * micPos->get();
-                posY = bounds.getHeight() * micDepth->get();
+                posX = micBounds.getWidth() * micPos->get();
+                posY = micBounds.getHeight() * micDepth->get();
+                clampPoints();
                 repaint();
             }
         }
@@ -113,8 +122,13 @@ private:
         void resized() override
         {
             auto bounds = getLocalBounds().reduced(2);
-            posX = bounds.getWidth() * micPos->get();
-            posY = bounds.getHeight() * micDepth->get();
+            auto width = bounds.getWidth();
+            auto height = bounds.getHeight();
+            micBounds = bounds.removeFromBottom(height * 0.65f).withWidth(width * 0.5f + (micWidth * 0.5f)).toFloat();
+            spkrBounds = bounds.toFloat();
+            posX = micBounds.getWidth() * micPos->get();
+            posY = micBounds.getHeight() * micDepth->get();
+            clampPoints();
         }
 
     private:
@@ -122,13 +136,18 @@ private:
         strix::FloatParameter *micPos, *micDepth;
         AudioProcessorValueTreeState &apvts;
 
+        std::unique_ptr<Drawable> mic_img, spkr_img;
+
         float posX = 0.f, posY = 0.f;
+        const float micWidth = 40.f;
+        Rectangle<float> micBounds, spkrBounds;
     };
 
     ChoiceMenu menu;
     std::unique_ptr<AudioProcessorValueTreeState::ComboBoxAttachment> menuAttach;
 
     std::unique_ptr<Drawable> cab_img;
+    Rectangle<float> cabBounds;
 
     MicComponent micComp;
 
@@ -166,7 +185,7 @@ public:
         if (paramID == "cabType")
         {
             setState();
-            repaint();
+            repaint(cabBounds.toNearestInt());
         }
     }
 
@@ -176,13 +195,11 @@ public:
         g.setColour(Colours::beige);
         g.drawRoundedRectangle(bounds, 5.f, 3.f);
 
-        auto img_bounds = bounds.removeFromLeft(bounds.getWidth() * 0.33f).reduced(bounds.getWidth() * 0.1f);
-
         g.setColour(Colours::darkgrey);
-        g.fillRoundedRectangle(img_bounds.toFloat().expanded(img_bounds.getWidth() * 0.2f), 5.f);
+        // g.fillRoundedRectangle(img_bounds.toFloat().expanded(img_bounds.getWidth() * 0.2f), 5.f);
 
         if (cab_img)
-            cab_img->drawWithin(g, img_bounds, RectanglePlacement::centred, 1.f);
+            cab_img->drawWithin(g, cabBounds, RectanglePlacement::centred, 1.f);
     }
 
     /**
@@ -219,8 +236,9 @@ public:
 
     void resized() override
     {
-        menu.setBounds(getLocalBounds().removeFromTop(getHeight() * 0.35f).removeFromRight(getWidth() * 0.5f).reduced(20, 5));
-
-        micComp.setBounds(getLocalBounds().removeFromBottom(getHeight() * 0.65f).removeFromRight(getWidth() * 0.5f));
+        menu.setBounds(getLocalBounds().removeFromTop(getHeight() * 0.35f).removeFromLeft(getWidth() * 0.5f).reduced(20, 5));
+        micComp.setBounds(getLocalBounds().removeFromRight(getHeight()));
+        auto bounds = getLocalBounds().reduced(2);
+        cabBounds = bounds.removeFromLeft(bounds.getWidth() * 0.5f).removeFromBottom(bounds.getHeight() * 0.65f).toFloat();
     }
 };
