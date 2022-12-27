0c7f3c842db10cc206dbac32fa8e6f57f92268ab
Arboreal Audio <arborealaudio@protonmail.com>
2022-12-27 10:53:07 -0500

    add a gain-based HP filter

--- Source/Processors/PreFilters.h
+++ Source/Processors/PreFilters.h
@@ -27,6 +27,9 @@ struct GuitarPreFilter : PreampProcessor
             h.prepare(spec);
         for (auto &l : sc_lp)
             l.prepare(spec);
+
+        dynHP.prepare(spec);
+        dynHP.setType(strix::FilterType::firstOrderHighpass);
     }
 
     void changeFilters()
@@ -37,21 +40,25 @@ struct GuitarPreFilter : PreampProcessor
             bp_coeffs = dsp::IIR::Coefficients<double>::makeHighPass(SR, 350.f);
             hs_coeffs = dsp::IIR::Coefficients<double>::makeHighShelf(SR, 350.f, 0.7f, 4.f);
             lp_coeffs = dsp::IIR::Coefficients<double>::makeLowPass(SR, 6200.f);
+            dynHP.setCutoffFreq(350.0);
             break;
         case Sunbeam:
             bp_coeffs = dsp::IIR::Coefficients<double>::makeHighPass(SR, 350.f); /*unused*/
             hs_coeffs = dsp::IIR::Coefficients<double>::makeHighShelf(SR, 750.f, 0.7f, 2.f);
             lp_coeffs = dsp::IIR::Coefficients<double>::makeFirstOrderLowPass(SR, 7200.f);
+            dynHP.setCutoffFreq(1200.0);
             break;
         case Moonbeam:
             bp_coeffs = dsp::IIR::Coefficients<double>::makeFirstOrderHighPass(SR, 150.f);
             hs_coeffs = dsp::IIR::Coefficients<double>::makeHighShelf(SR, 175.f, 0.666f, 4.f);
-            lp_coeffs = dsp::IIR::Coefficients<double>::makeLowPass(SR, 6200.f);
+            lp_coeffs = dsp::IIR::Coefficients<double>::makeLowPass(SR, 7500.f);
+            dynHP.setCutoffFreq(350.0);
             break;
         case XRay:
-            bp_coeffs = dsp::IIR::Coefficients<double>::makeFirstOrderHighPass(SR, 350.f);
-            hs_coeffs = dsp::IIR::Coefficients<double>::makeHighShelf(SR, 175.f, 0.7f, 3.f);
+            bp_coeffs = dsp::IIR::Coefficients<double>::makeHighPass(SR, 450.f);
+            hs_coeffs = dsp::IIR::Coefficients<double>::makeHighShelf(SR, 350.f, 0.7f, 3.f);
             lp_coeffs = dsp::IIR::Coefficients<double>::makeLowPass(SR, 5500.f);
+            dynHP.setCutoffFreq(500.0);
             break;
         }
 
@@ -85,6 +92,7 @@ struct GuitarPreFilter : PreampProcessor
                 {
                     in[i] = hiShelf[ch].processSample(in[i]);
                     in[i] = sc_lp[ch].processSample(in[i]);
+                    in[i] += dynHPGain * dynHP.processSample(ch, in[i]);
                 }
             }
         }
@@ -96,6 +104,7 @@ struct GuitarPreFilter : PreampProcessor
                 for (int i = 0; i < block.getNumSamples(); ++i)
                 {
                     in[i] = bandPass[ch].processSample(in[i]);
+                    in[i] += dynHPGain * dynHP.processSample(ch, in[i]);
                 }
             }
         }
@@ -103,6 +112,7 @@ struct GuitarPreFilter : PreampProcessor
 #else
     void process(dsp::AudioBlock<double> &block) override
     {
+        auto dynHPGain = 1.f / jmax(inGain, 1.f);
         if (*hiGain)
         {
             for (int ch = 0; ch < block.getNumChannels(); ++ch)
@@ -112,6 +122,7 @@ struct GuitarPreFilter : PreampProcessor
                 {
                     in[i] = hiShelf[ch].processSample(in[i]);
                     in[i] = sc_lp[ch].processSample(in[i]);
+                    in[i] += dynHPGain * dynHP.processSample(ch, in[i]);
                 }
             }
         }
@@ -123,18 +134,22 @@ struct GuitarPreFilter : PreampProcessor
                 for (int i = 0; i < block.getNumSamples(); ++i)
                 {
                     in[i] = bandPass[ch].processSample(in[i]);
+                    in[i] += dynHPGain * dynHP.processSample(ch, in[i]);
                 }
             }
         }
     }
 #endif
 
-    strix::BoolParameter* hiGain = nullptr;
+    strix::BoolParameter *hiGain = nullptr;
     GuitarMode type;
+    // the input gain parameter, used for dynHP
+    float inGain = 1.f;
 
 private:
     std::array<dsp::IIR::Filter<T>, 2> bandPass, hiShelf, sc_lp;
     dsp::IIR::Coefficients<double>::Ptr bp_coeffs, hs_coeffs, lp_coeffs;
+    strix::SVTFilter<T> dynHP;
 
     double SR = 44100.0;
 };
@@ -150,6 +165,8 @@ struct BassPreFilter : PreampProcessor
         filter.resize(spec.numChannels);
         for (auto &f : filter)
             f.prepare(spec);
+        dynHP.prepare(spec);
+        dynHP.setType(strix::FilterType::firstOrderHighpass);
         changeFilters();
     }
 
@@ -166,43 +183,56 @@ struct BassPreFilter : PreampProcessor
         case Cobalt:
             for (auto &f : filter)
                 f.coefficients = dsp::IIR::Coefficients<double>::makePeakFilter(SR, 650.0, 1.0, 0.5);
+            dynHP.setCutoffFreq(600.0);
             break;
         case Emerald:
             for (auto &f : filter)
                 f.coefficients = dsp::IIR::Coefficients<double>::makePeakFilter(SR, 1000.0, 0.7, 0.5);
+            dynHP.setCutoffFreq(900.0);
             break;
         case Quartz:
             for (auto &f : filter)
                 f.coefficients = dsp::IIR::Coefficients<double>::makePeakFilter(SR, 1500.0, 0.66, 1.2);
+            dynHP.setCutoffFreq(1200.0);
             break;
         }
     }
 #if USE_SIMD
     void process(strix::AudioBlock<vec> &block) override
     {
+        float dynHPGain = 1.f / jmax(inGain, 1.f);
         for (size_t ch = 0; ch < block.getNumChannels(); ++ch)
         {
             auto in = block.getChannelPointer(ch);
             for (size_t i = 0; i < block.getNumSamples(); ++i)
+            {
                 in[i] = filter[ch].processSample(in[i]);
+                in[i] += dynHPGain * dynHP.processSample(ch, in[i]);
+            }
         }
     }
 #else
     void process(dsp::AudioBlock<double> &block) override
     {
+        float dynHPGain = 1.f / jmax(inGain, 1.f);
         for (size_t ch = 0; ch < block.getNumChannels(); ++ch)
         {
             auto in = block.getChannelPointer(ch);
             for (size_t i = 0; i < block.getNumSamples(); ++i)
+            {
                 in[i] = filter[ch].processSample(in[i]);
+                in[i] += dynHPGain * dynHP.processSample(ch, in[i]);
+            }
         }
     }
 #endif
     strix::BoolParameter *hiGain = nullptr;
+    float inGain = 1.f;
     BassMode type;
 
 private:
     std::vector<dsp::IIR::Filter<T>> filter;
+    strix::SVTFilter<T> dynHP;
 
     double SR = 44100.0;
 };
