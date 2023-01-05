ec065dabcb31467831ba9f8888a6a53850af9a6c
Arboreal Audio <arborealaudio@protonmail.com>
2023-01-05 14:35:35 -0500

    fix some segfaulting conditions

--- Source/Processors/EmphasisFilters.h
+++ Source/Processors/EmphasisFilters.h
@@ -40,23 +40,32 @@ struct EmphasisFilter
     template <typename Block>
     void processIn(Block &block)
     {
-        wetBlock.copyFrom(block);
+        const auto numSamples = block.getNumSamples();
+        FloatVectorOperations::copy(wetBlock.getChannelPointer(0), block.getChannelPointer(0), numSamples);
+        if (block.getNumChannels() > 1 && wetBlock.getNumChannels() > 1)
+            FloatVectorOperations::copy(wetBlock.getChannelPointer(1), block.getChannelPointer(1), numSamples);
 
         auto gain = Decibels::decibelsToGain(amount->get()) - 1.f; /* -1 bc of wet/dry schema */
         if (lastFreq != *freq)
             fIn.setCutoffFreq(freq->get());
 
-        auto subBlock = wetBlock.getSubBlock(0, block.getNumSamples());
-        fIn.processBlock(subBlock);
+        fIn.processChannel(wetBlock.getChannelPointer(0), 0, numSamples);
+        if (block.getNumChannels() > 1 && wetBlock.getNumChannels() > 1)
+            fIn.processChannel(wetBlock.getChannelPointer(1), 1, numSamples);
 
-        subBlock *= gain;
-        block.add(subBlock);
+        wetBlock *= gain;
+        FloatVectorOperations::add(block.getChannelPointer(0), wetBlock.getChannelPointer(0), numSamples);
+        if (block.getNumChannels() > 1 && wetBlock.getNumChannels() > 1)
+            FloatVectorOperations::add(block.getChannelPointer(1), wetBlock.getChannelPointer(1), numSamples);
     }
 
     template <typename Block>
     void processOut(Block &block)
     {
-        block.subtract(wetBlock.getSubBlock(0, block.getNumSamples()));
+        FloatVectorOperations::subtract(block.getChannelPointer(0), wetBlock.getChannelPointer(0), block.getNumSamples());
+        if (block.getNumChannels() > 1 && wetBlock.getNumChannels() > 1)
+            FloatVectorOperations::subtract(block.getChannelPointer(1), wetBlock.getChannelPointer(1), block.getNumSamples());
+        // block.subtract(wetBlock.getSubBlock(0, block.getNumSamples()));
         // wetBlock.copyFrom(block);
 
         // auto gain = (Decibels::decibelsToGain(-1.f * amount->get())) - 1.f;
@@ -79,5 +88,5 @@ private:
     HeapBlock<char> heap;
     dsp::AudioBlock<T> wetBlock;
     double SR = 44100.0;
-    float lastFreq;
+    float lastFreq = 1.f;
 };
