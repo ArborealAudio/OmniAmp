dbcac6816cc235dd5a5e7690675531e37e077536
Arboreal Audio <arborealaudio@protonmail.com>
2023-03-20 17:36:02 -0400

    Refactor DownloadManager for new json enpoint

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -3,12 +3,12 @@
 #include "PluginProcessor.h"
 
 #define SITE_URL "https://arborealaudio.com"
-    #if JUCE_WINDOWS
-        #define DL_BIN "Gamma-windows.exe"
-    #elif JUCE_MAC
-        #define DL_BIN "Gamma-mac.dmg"
-    #elif JUCE_LINUX
-        #define DL_BIN "Gamma-linux.tar.gz"
+#if JUCE_WINDOWS
+    #define DL_BIN "OmniAmp-windows.exe"
+#elif JUCE_MAC
+    #define DL_BIN "OmniAmp-mac.dmg"
+#elif JUCE_LINUX
+    #define DL_BIN "OmniAmp-linux.tar.xz"
 #endif
 
 static strix::UpdateResult dlResult;
