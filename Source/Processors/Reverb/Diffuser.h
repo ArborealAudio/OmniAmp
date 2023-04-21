e5064312d024b601c7ed7f3a71d8855a500932a9
ArborealAudio <arborealaudio@protonmail.com>
2023-04-21 10:07:57 -0400

    Improve reverb code a bit Don't allocate a vector of delayed samples for every sample in the buf Add some comments to understand matrices better

--- Source/Processors/Reverb/Diffuser.h
+++ Source/Processors/Reverb/Diffuser.h
@@ -88,18 +88,16 @@ struct Diffuser
         // }
         for (auto i = 0; i < block.getNumSamples(); ++i)
         {
-            std::vector<T> vec;
-
             for (auto ch = 0; ch < channels; ++ch)
             {
                 delay[ch].pushSample(0, block.getSample(ch, i));
-                vec.push_back(delay[ch].popSample(0));
+                delayed[ch] = (delay[ch].popSample(0));
             }
 
-            MixMatrix<channels>::processHadamardMatrix(vec.data());
+            MixMatrix<channels>::processHadamardMatrix(delayed.data());
 
             for (auto ch = 0; ch < channels; ++ch)
-                block.getChannelPointer(ch)[i] = vec[ch];
+                block.getChannelPointer(ch)[i] = delayed[ch];
         }
         for (auto ch = 0; ch < channels; ++ch)
             if (invert[ch])
@@ -113,18 +111,16 @@ struct Diffuser
         {
             changeDelay();
 
-            std::vector<T> vec;
-
             for (auto ch = 0; ch < channels; ++ch)
             {
                 delay[ch].pushSample(0, block.getSample(ch, i));
-                vec.push_back(delay[ch].popSample(0));
+                delayed[ch] = (delay[ch].popSample(0));
             }
 
-            MixMatrix<channels>::processHadamardMatrix(vec.data());
+            MixMatrix<channels>::processHadamardMatrix(delayed.data());
 
             for (auto ch = 0; ch < channels; ++ch)
-                block.getChannelPointer(ch)[i] = vec[ch];
+                block.getChannelPointer(ch)[i] = delayed[ch];
         }
         for (auto ch = 0; ch < channels; ++ch)
             if (invert[ch])
@@ -136,6 +132,7 @@ struct Diffuser
 private:
     std::array<dsp::DelayLine<T, dsp::DelayLineInterpolationTypes::Linear>, channels> delay;
     std::array<int, channels> randDelay;
+    std::array<T, channels> delayed;
     const int64_t seed;
     std::vector<bool> invert;
     double SR = 44100.0;
